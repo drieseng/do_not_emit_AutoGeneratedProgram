3f5e60597d2c7a438ae26253eafd72de41ca9bf4
Gert Driesen <gert.driesen@infrabel.be>
2024-12-06 16:07:53 +0100

    Sample project to reproduce the fact that the Microsoft.Testing.Platform emits an AutoGeneratedProgram class even though the GenerateTestingPlatformEntryPoint MSBuild property is set to 'false'.

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void AlwaysTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
