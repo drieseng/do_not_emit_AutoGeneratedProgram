2a00066dab2a1a1a851790afa66eff108852ff26
Gert Driesen <gert.driesen@infrabel.be>
2024-12-06 17:15:55 +0100

    Enable analyzers, use .NET 9.0 and set GenerateProgramFile to false.

--- Class1.cs
+++ Class1.cs
@@ -3,7 +3,9 @@
 namespace UnitTests
 {
     [TestClass]
+#pragma warning disable CA1515 // Consider making public types internal
     public class Class1
+#pragma warning restore CA1515 // Consider making public types internal
     {
         [TestMethod]
         public void AlwaysTrue()
